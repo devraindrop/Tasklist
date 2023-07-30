d4610cff373acb7d01dd9c9425041c8f30bcfcbe
devraindrop <raindrop221g@gmail.com>
2023-07-31 00:31:37 +0500

    api mockup working

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import {
-    atom,
-    useRecoilState,
-  } from 'recoil';
+import { atom, useRecoilState } from 'recoil';
 import Menu from "./menu";
+import {validateUserCredentials} from '../mockapi/api'
+
 
 export const userGroupState = atom({
     key: 'userGroupState', // unique ID (with respect to other atoms/selectors)
@@ -23,24 +22,27 @@ const Login = () => {
 
     const [, setUserGroup] = useRecoilState(userGroupState);
 
-    const userCredentials = [
+/*     const userCredentials = [
         { username: 'user1', password: 'password1', group: 'group1' },
         { username: 'user2', password: 'password2', group: 'group1' },
         { username: 'user3', password: 'password3', group: 'group2' },
         { username: 'user4', password: 'password4', group: 'group2' },
         { username: 'user5', password: 'password5', group: 'group3' },
-      ];
+      ]; */
 
-      const handleLogin = () => {
-        const matchedUser = userCredentials.find(
+      const handleLogin = async () => {
+/*         const matchedUser = userCredentials.find(
           (user) => user.username === username && user.password === password
-        );
-    
+        ); */
+
+        const matchedUser = await validateUserCredentials(username, password);
+
         if (matchedUser) {
           setIsLoggedIn(true);
-          setErrorMessage('');
+          setErrorMessage("");
 
-          const ug = userCredentials.find(user => user.username === username)?.group;
+          //const ug = userCredentials.find(user => user.username === username)?.group;
+          const ug = matchedUser.group;
           console.log(ug)
           setUserGroup(ug);
           
@@ -56,58 +58,61 @@ const Login = () => {
       };
 
 
-    return ( 
-        <div>
-          <Menu
-            onLogout={handleLogout}
-            isLoggedIn={isLoggedIn}
-          />            
+  return ( 
+      <div>
+        <Menu
+          onLogout={handleLogout}
+          isLoggedIn={isLoggedIn}
+        />
+        {errorMessage !== '' && 
+            <div className="text-red-500 mt-4">
+              {errorMessage}
+            </div>
+        }
 
-{!isLoggedIn ? (
-        <div className="bg-gray-100 mx-auto px-4 py-8">
-          <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-semibold mb-4">User Login</h2>
-            <div className="mb-4">
-              <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
-                Username:
-              </label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
+        {!isLoggedIn ? (
+          <div className="bg-gray-100 mx-auto px-4 py-8">
+            <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
+              <h2 className="text-2xl font-semibold mb-4">User Login</h2>
+              <div className="mb-4">
+                <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+                  Username:
+                </label>
+                <input
+                  type="text"
+                 id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+                 Password:
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+               />
+              </div>
+              <button
+                onClick={handleLogin}
+                className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Login
+              </button>
             </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
-                Password:
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <button
-              onClick={handleLogin}
-              className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Login
-            </button>
-            {errorMessage && <p className="text-red-500 mt-4">{errorMessage}</p>}
           </div>
-        </div>
-      ) : (
-        <>
-          {/* Show content for logged-in user */}
-        </>
-      )}
-
-        </div>
-     );
+        ) : (
+          <>
+            {/* Show content for logged-in user */}
+          </>
+        )}
+      </div>
+  );
 }
  
 export default Login;
---
+++ src/mockapi/api.js
@@ -0,0 +1,19 @@
+export const validateUserCredentials = async (username, password) => {
+    // Simulate API latency with setTimeout
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  
+    const userCredentials = [
+      { username: 'user1', password: 'password1', group: 'group1' },
+      { username: 'user2', password: 'password2', group: 'group1' },
+      { username: 'user3', password: 'password3', group: 'group2' },
+      { username: 'user4', password: 'password4', group: 'group2' },
+      { username: 'user5', password: 'password5', group: 'group3' },
+    ];
+  
+    const matchedUser = userCredentials.find(
+      (user) => user.username === username && user.password === password
+    );
+  
+    return matchedUser;
+  };
+  
